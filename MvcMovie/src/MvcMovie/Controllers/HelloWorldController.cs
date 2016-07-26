71dc1efcbe0262a0c79b25cd24af4e4c8e30f749
Patrick Dobson <pddobso@gmail.com>
2016-07-26 11:42:52 -0400

    finished view sections of tutorial, models next. commiting changes.

--- MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -7,20 +7,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.HtmlEncode($"Hello {name}, numTimes: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
