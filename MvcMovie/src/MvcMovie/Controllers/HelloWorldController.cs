03f22ecdc8953cf97534c40779ddc1a6e5dbb329
Patrick Dobson <pddobso@gmail.com>
2016-07-26 10:30:56 -0400

    initial commit of mvc movie start

---
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.HtmlEncode($"Hello {name}, numTimes: {numTimes}");
+        }
+    }
+}
