20e621462fe2317b556ad26446ffc2af6dbe350e
Patrick Dobson <pddobso@gmail.com>
2016-07-28 12:58:54 -0400

    MvcMovie project finished. Tested working 100

--- MvcMovie/src/MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/src/MvcMovie/Controllers/HomeController.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
         {
             return View();
         }
